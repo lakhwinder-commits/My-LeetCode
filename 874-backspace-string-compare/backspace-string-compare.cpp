ea0c99e8f5c1978e2b164dc1c5588405397ff3ea
Lakhwinder Singh <lakhwinder32105@gmail.com>
2026-07-19 15:18:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.30%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<int> s1;
+        stack<int> t1;
+        for(char c:s){
+            if(c=='#'){
+                if(!s1.empty()){
+                    s1.pop();
+                }
+            }
+            else {
+                s1.push(c);
+            }
+
+        }
+        for(char c:t){
+            if(c=='#'){
+                if(!t1.empty()){
+                    t1.pop();
+                }
+            }
+            else {
+                t1.push(c);
+            }
+            
+        }
+        if(s1==t1) return true;
+        else return false;
+    }
+};
